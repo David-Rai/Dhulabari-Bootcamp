870d44aab15f2131153507727aa8a2eb3d6892dc
david-rai <davidkhaling123@gmail.com>
2025-10-15 22:12:56 +0545

    dynamic made

---
+++ docs/app.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded",()=>{
+    const sites=
+[
+    's1','s2','s3','s4'
+]
+let main_container=document.querySelector('.main-container')
+console.log(main_container)
+
+const html=sites.map(s=>(
+`
+      <div class="site-container">
+            <h2><i class="fas fa-user"></i>${s}</h2>
+            <p><i class="fas fa-link"></i> 
+            <a href="${window.location.origin}/sites/${s}"
+             target="_blank">${window.location.origin}/sites/${s}</p>
+        </div>`
+))
+
+main_container.innerHTML=html
+})
