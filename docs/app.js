a0d61b4ccec07108011447b95f5c2f876d029cd0
david-rai <davidkhaling123@gmail.com>
2025-10-16 08:45:45 +0545

    done

--- docs/app.js
+++ docs/app.js
@@ -1,20 +1,19 @@
-window.addEventListener("DOMContentLoaded",()=>{
-    const sites=
-[
-    'ram','shyam','hari','prashad'
-]
-let main_container=document.querySelector('.main-container')
-console.log(main_container)
+window.addEventListener("DOMContentLoaded", () => {
+  const sites = ['ram', 'shyam', 'hari', 'prashad'];
+  const main_container = document.querySelector('.main-container');
+  console.log(main_container);
 
-const html=sites.map(s=>(
-`
+  const html = sites.map(s => `
       <div class="site-container">
-            <h2><i class="fas fa-user"></i>${s}</h2>
-            <p><i class="fas fa-link"></i> 
-            <a href="${window.location.origin}/sites/${s}"
-             target="_blank">${window.location.origin}/sites/${s}</p>
-        </div>`
-))
+        <h2><i class="fas fa-user"></i> ${s}</h2>
+        <p>
+          <i class="fas fa-link"></i> 
+          <a href="${window.location.origin}/sites/${s}" target="_blank">
+            ${window.location.origin}/sites/${s}
+          </a>
+        </p>
+      </div>
+  `);
 
-main_container.innerHTML=html
-})
+  main_container.innerHTML = html.join('');
+});
