f1450b5ce7b39f5e781046f015cbf0b3aec816f9
david-rai <davidkhaling123@gmail.com>
2025-10-16 09:20:38 +0545

    done

--- docs/app.js
+++ docs/app.js
@@ -8,8 +8,8 @@ window.addEventListener("DOMContentLoaded", () => {
         <h2><i class="fas fa-user"></i> ${s}</h2>
         <p>
           <i class="fas fa-link"></i> 
-          <a href="${window.location.origin}/docs/sites/${s}" target="_blank">
-            ${window.location.origin}/docs/sites/${s}
+          <a href="${window.location.origin}/sites/${s}" target="_blank">
+            ${window.location.origin}/sites/${s}
           </a>
         </p>
       </div>
